3fb13b22007afa1a2f42d63d1f48480834f0f9a9
Joachim Lous <joachim@lous.org>
2015-05-26 12:30:37 +0200

    Clean up header and rev version.

--- IntraSane.tamper.js
+++ IntraSane.tamper.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         IntraSane
-// @namespace    https://github.com/steria/IntraSane
-// @version      1.0
 // @description  Gui-tweaks for SopraSteria intranett
+// @version      1.1
 // @author       Joachim Lous
+// @namespace    https://github.com/steria/IntraSane
+// @homepage     https://github.com/steria/IntraSane
+// @downloadURL  https://github.com/steria/IntraSane/raw/master/IntraSane.tamper.js
 // @match        https://intranet.steria.no/*
 // @require      http://cherne.net/brian/resources/jquery.hoverIntent.minified.js
+// @grant        none
 // ==/UserScript==
 
 var roeNedMegamenyenLitt = function(){
